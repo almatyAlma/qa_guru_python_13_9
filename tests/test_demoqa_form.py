1f6aa79ae2fb66751a85b80c94bd30f90c604fe4
Bekbergenova Alma <alma02071988@gmail.com>
2024-06-17 18:40:07 +0500

    Исправление1

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -7,30 +7,30 @@ def test_registration_form():
     registration_page.open()
 
     (
-        registration_page.fill_first_name("Test")
-        .fill_second_name("Testov")
+        registration_page.fill_first_name("Alma")
+        .fill_second_name("Bekbergenova")
         .fill_email("test@test.com")
-        .select_gender("test")
+        .select_gender("Male")
         .fill_phone_number("1234567890")
-        .fill_date_of_birth("July", "1988", "02")
-        .fill_subjects("Computer Science")
-        .fill_hobbies("Sing")
+        .fill_date_of_birth("July", "1991", "02")
+        .fill_subjects("QA")
+        .fill_hobbies("Sports")
         .upload_picture("orig.jpg")
-        .fill_current_address("test test")
-        .fill_state("test")
-        .fill_city("test")
+        .fill_current_address("Abay")
+        .fill_state("Haryana")
+        .fill_city("Karnal")
         .submit_form()
     )
 
     registration_page.should_registered_user_info_with(
-        "Test Testov",
+        "Alma Bekbergenova",
         "test@test.com",
-        "test",
+        "Male",
         "1234567890",
-        "02 July,1988",
-        "test test",
-        "Sing",
+        "02 July,1991",
+        "Economics",
+        "Sports",
         "orig.jpg",
-        "test test",
-        "test test",
+        "Abay",
+        "Haryana Karnal",
     )
